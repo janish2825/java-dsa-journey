9b2b68d8b86acf7aa41b103622b7ab0a5aae5625
janish2825 <janish200828@gmail.com>
2026-09-11 18:52:29 +0530

    Add Day 1 Q2 solution
    Implement a program to print all even numbers from 1 to N.

---
+++ Day-01/Q2_EvenNumbers.java
@@ -0,0 +1,18 @@
+// Q2: Print all even numbers from 1 to N
+
+import java.util.Scanner;
+
+class Main{
+    public static void main (String[] args) {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+        }
+    }
+}
